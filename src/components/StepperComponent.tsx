adf36f6977dac711716bd37c58ce4f65ad04ff68
MasNopalll <72863301+MasNopalll@users.noreply.github.com>
2023-06-07 14:12:50 +0700

    Update StepperComponent.tsx

--- src/components/StepperComponent.tsx
+++ src/components/StepperComponent.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
 /*
+import * as React from 'react';
+
 import Box from '@mui/material/Box';
 import { useTheme } from '@mui/material/styles';
 import MobileStepper from '@mui/material/MobileStepper';
